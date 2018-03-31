e75df7488ee69914650da23d2866cd24c432ca41
shine <shinemotec@gmail.com>
2018-03-31 15:38:36 +0800

    initial repo

--- rocksdb.c
+++ rocksdb.c
@@ -1,41 +1,14 @@
 /* rocksdb extension for PHP */
 
 #ifdef HAVE_CONFIG_H
-# include "config.h"
+#include "config.h"
 #endif
 
 #include "php.h"
 #include "ext/standard/info.h"
 #include "php_rocksdb.h"
 
-/* {{{ void rocksdb_test1()
- */
-PHP_FUNCTION(rocksdb_test1)
-{
-	ZEND_PARSE_PARAMETERS_NONE();
 
-	php_printf("The extension %s is loaded and working!\r\n", "rocksdb");
-}
-/* }}} */
-
-/* {{{ string rocksdb_test2( [ string $var ] )
- */
-PHP_FUNCTION(rocksdb_test2)
-{
-	char *var = "World";
-	size_t var_len = sizeof("World") - 1;
-	zend_string *retval;
-
-	ZEND_PARSE_PARAMETERS_START(0, 1)
-		Z_PARAM_OPTIONAL
-		Z_PARAM_STRING(var, var_len)
-	ZEND_PARSE_PARAMETERS_END();
-
-	retval = strpprintf(0, "Hello %s", var);
-
-	RETURN_STR(retval);
-}
-/* }}}*/
 
 /* {{{ PHP_RINIT_FUNCTION
  */
@@ -59,21 +32,10 @@ PHP_MINFO_FUNCTION(rocksdb)
 }
 /* }}} */
 
-/* {{{ arginfo
- */
-ZEND_BEGIN_ARG_INFO(arginfo_rocksdb_test1, 0)
-ZEND_END_ARG_INFO()
-
-ZEND_BEGIN_ARG_INFO(arginfo_rocksdb_test2, 0)
-	ZEND_ARG_INFO(0, str)
-ZEND_END_ARG_INFO()
-/* }}} */
-
 /* {{{ rocksdb_functions[]
  */
 static const zend_function_entry rocksdb_functions[] = {
-	PHP_FE(rocksdb_test1,		arginfo_rocksdb_test1)
-	PHP_FE(rocksdb_test2,		arginfo_rocksdb_test2)
+	
 	PHP_FE_END
 };
 /* }}} */
