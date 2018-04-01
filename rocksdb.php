47a42ca161965f52903055e20cabc367c91fbf1b
shine <shinemotec@gmail.com>
2018-04-01 22:25:55 +0800

    0.10-dev

--- rocksdb.php
+++ rocksdb.php
@@ -9,6 +9,8 @@ $connector = new RDB();
 $connector->connect();
 var_dump($connector->enableBackup());
 var_dump($connector->restoreLastBackup());
+var_dump($connector->newBackup());
 var_dump($connector->get("arr"));
 var_dump($connector->delete("arr"));
+var_dump($connector->put("arr", serialize(['1' => "Happy Birthday"])));
 var_dump($connector->get("arr"));
